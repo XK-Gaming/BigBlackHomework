81c62fe6175b0460fce056adf9d1219f9e670200
huylinh397 <135983579+huylinh397@users.noreply.github.com>
2026-04-23 11:32:47 +0700

    Delete BBC/du_an_lon directory

--- BBC/du_an_lon/src/main/java/trang_chu/du_an_lon/Launcher.java
+++
@@ -1,9 +0,0 @@
-package trang_chu.du_an_lon;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(Main.class, args);
-    }
-}
